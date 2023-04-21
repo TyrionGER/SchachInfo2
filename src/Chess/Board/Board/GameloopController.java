ea1c66c44eb411539c77b6c941f5b7119a8a26df
Mehmet Yildiz <Mehmet.Yildiz@Student.Reutlingen-University.DE>
2023-04-21 19:54:58 +0200

    Changes by lenna

--- src/Chess/Board/Board/GameloopController.java
+++ src/Chess/Board/Board/GameloopController.java
@@ -70,21 +70,7 @@ public class GameloopController {
 
                 //todo Matt ausführen.
                 if(istMatt){
-                    int eingabe = 0;
-                    System.out.println("Glueckwunsch " + getCurrentColor() + " hat die Partie Gewonnen!");
-                    System.out.println("Menue:");
-                    System.out.println("1). Revanche");
-                    System.out.println("2). Applikation Beenden");
-                    switch (eingabe){
-                        case 1:
-                            //Schachfeld neu initialisieren
-                            //die folgenden 2 zeilen sorgen dafür das die Konsole gecleared wird
-                            System.out.print("\033[H\033[2J");
-                            System.out.flush();
-                        case 2:
-                            //Beende die Applikation
-                        default:
-                            System.out.println("Unguelige Eingabe");
+^^
                     }
                 }
             }
