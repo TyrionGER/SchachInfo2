697dfef3ccc6d984a99acd8cb4e4092edd80fcb6
Lennartlaptop <lennart.frank@student.reutlingen-university.de>
2023-06-26 06:56:52 +0200

    Lexikon von Sascha und Samir

--- src/Chess/Board/Board/MainFrame.java
+++ src/Chess/Board/Board/MainFrame.java
@@ -135,88 +135,78 @@ public class MainFrame extends JFrame {
 
 
         // Seite 1: Schritte für Schach
-        String schritteText = """
-                Vorbereitung
-
-                Schritt 1: Aufbau des Schachbretts
-                Am Anfang des Spiels wird das Schachbrett so aufgebaut, dass jeder Spieler das weiße (oder hellere) Feld unten rechts hat.
-                Die Schachfiguren werden jedes Mal auf die gleiche Weise angeordnet. Die zweite Reihe wird mit Bauern gefüllt. Die Türme kommen in die Ecken,
-                dann die Springer neben ihnen, gefolgt von den Läufern und schließlich die Dame,
-                die immer auf ihr entsprechendes Farbfeld gestellt wird (weiße Dame auf weiß, schwarze Dame auf schwarz), und der König auf das verbleibende Feld.
-
-                Schritt 2: Die Züge der Figuren
-                Jede Schachfigur hat ihre eigenen Bewegungsregeln. Der Bauer zieht ein Feld nach vorne, kann jedoch beim ersten Zug auch zwei Felder vorrücken.
-                Er schlägt diagonal und kann bei der Umwandlung zur Dame, falls er die gegnerische Grundreihe erreicht, jede Figur wählen. Die Türme können horizontal und vertikal ziehen,
-                solange kein Hindernis im Weg ist. Die Springer ziehen in einem L-förmigen Muster, zwei Felder geradeaus und dann ein Feld zur Seite.
-                Die Läufer ziehen diagonal in alle Richtungen. Die Dame kann in alle Richtungen ziehen, horizontal, vertikal und diagonal.
-                Der König kann ein Feld in jede Richtung ziehen, aber nicht auf ein Feld, das von einer gegnerischen Figur angegriffen wird.
-                Es gibt auch spezielle Züge wie die Rochade, bei der der König mit einem Turm tauscht, und die en passant Regel, bei der ein Bauer einen anderen Bauer schlägt.
-
-                Schritt 3: Ziel des Spiels
-                Das Ziel des Schachspiels besteht darin, den gegnerischen König in eine Position zu bringen, aus der er nicht mehr entkommen kann,
-                das heißt, ihn im Schachmatt zu setzen. Dies geschieht, wenn der König im Schach steht und keine legale Zugmöglichkeit mehr hat, um dem Angriff zu entkommen.
-                Der Spieler, der den gegnerischen König schachmatt setzt, gewinnt die Partie.
-
-                Schritt 4: Sonderregeln
-                Neben den grundlegenden Zügen gibt es noch einige Sonderregeln im Schach. Dazu gehören die Rochade, bei der der König und ein Turm gleichzeitig gezogen werden,
-                um den König in eine sicherere Position zu bringen. Die en passant Regel erlaubt es einem Bauern, einen gegnerischen Bauern zu schlagen,
-                der gerade zwei Felder vorgerückt ist. Des Weiteren gibt es noch die Umwandlung, bei der ein Bauer die gegnerische Grundreihe erreicht und in eine andere Figur umgewandelt werden kann.
-                Schließlich gibt es noch das Remis, bei dem das Spiel unentschieden endet, zum Beispiel bei Patt (der König steht nicht im Schach, aber der Spieler hat keinen legalen Zug).
+        String basisRegelnText = """
+                
+                Basis-Regeln:
+                
+                Das Schachbrett:
+                Das Schachbrett besteht aus 64 Feldern, abwechselnd hellen (weißen) und dunklen (schwarzen) Feldern. 
+                Es hat 8 Reihen (waagerecht) und 8 Linien (senkrecht). Die Spieler sitzen einander gegenüber, wobei das rechte Eckfeld des Schachbretts für jeden Spieler ein weißes Feld ist. 
+                                                                                                                                                           
+                Die Schachfiguren: 
+                Jeder Spieler hat 16 Schachfiguren, die zu Beginn des Spiels in einer bestimmten Formation auf dem Schachbrett platziert sind. 
+                Die Figuren bestehen aus dem König, der Dame, zwei Türmen, zwei Springern, zwei Läufern und acht Bauern.
+                                                                                                                                                                                            
+                Das Ziel des Spiels: 
+                Das Ziel des Schachspiels ist es, den gegnerischen König in eine Position zu bringen, in der er im nächsten Zug bedroht (Schach) ist und keine legale Zugmöglichkeit hat, 
+                um dem Schach zu entkommen. Dieser Zustand wird "Schachmatt" genannt, und der Spieler, dessen König schachmatt gesetzt wurde, verliert das Spiel.
+                                                                                                                                                                                            
+                Die Zugregeln: 
+                Jede Schachfigur hat spezifische Bewegungsmöglichkeiten. Der König kann ein Feld in jede Richtung bewegen.
+                Die Dame kann sich horizontal, vertikal und diagonal über das gesamte Schachbrett bewegen. Die Türme bewegen sich horizontal und vertikal.
+                Die Läufer bewegen sich diagonal. Die Springer bewegen sich in einem "L"-förmigen Sprungmuster. Die Bauern bewegen sich vorwärts, aber sie schlagen diagonal.
+                                                                                                                                                                                            
+                Schlagen: 
+                Wenn eine Figur auf ein Feld zieht, das von einer gegnerischen Figur besetzt ist, wird die gegnerische Figur geschlagen und aus dem Spiel genommen.
+                                                                                                                                                                                            
+                Rochade: 
+                Die Rochade ist ein spezieller Zug, bei dem der König und ein Turm gleichzeitig bewegt werden. Sie ermöglicht es dem König, 
+                sich in eine sicherere Position zu begeben und den Turm zu entwickeln.
+                                                                                                                                                                                           
+                En Passant: 
+                Dies ist ein spezieller Zug, der beim Vorbeiziehen eines Bauern ermöglicht wird, der zwei Felder von seinem Startfeld aus vorrückt und einen gegnerischen Bauern passiert.
+                                                                                                                                                                                            
+                Patt: 
+                Wenn ein Spieler keinen legalen Zug mehr machen kann und sein König nicht im Schach steht, endet das Spiel in einem Patt. In diesem Fall endet das Spiel unentschieden.
 
                 """;
 
-        JTextArea schritteTextArea = new JTextArea(schritteText);
-        schritteTextArea.setEditable(false);
-        schritteTextArea.setLineWrap(true);
-        schritteTextArea.setWrapStyleWord(true);
-        schritteTextArea.setBackground(new Color(255, 255, 0, 128)); // Hintergrundfarbe festlegen
-        JScrollPane schritteScrollPane = new JScrollPane(schritteTextArea);
+        JTextArea basisRegelnTextArea = new JTextArea(basisRegelnText);
+        basisRegelnTextArea.setEditable(false);
+        basisRegelnTextArea.setLineWrap(true);
+        basisRegelnTextArea.setWrapStyleWord(true);
+        basisRegelnTextArea.setBackground(new Color(43, 43, 43)); // Hintergrundfarbe festlegen
+        basisRegelnTextArea.setForeground(Color.white);
+        JScrollPane schritteScrollPane = new JScrollPane(basisRegelnTextArea);
         schritteScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         cardPanel.add(schritteScrollPane);
 
-        // Seite 2: Regeln des Schachs
-        String regelnText = """
-                Regeln des Schachs:
-                Schach ist ein strategisches Brettspiel zwischen zwei Spielern, bei dem das Ziel darin besteht, den gegnerischen König zu schlagen.
-                Die Spieler ziehen abwechselnd und dürfen jeweils eine ihrer Figuren bewegen. Eine Figur kann auf ein freies Feld ziehen oder eine gegnerische Figur schlagen.
-                Der König darf nicht ins Schach gezogen werden, das heißt, er darf nicht auf ein Feld ziehen, das von einer gegnerischen Figur angegriffen wird.
-                Wenn ein Spieler seinen König nicht vor einem Schachgebot schützen kann, ist er im Schach.
-                Das Spiel kann auf verschiedene Arten enden: durch Schachmatt, Remis (Unentschieden) oder Aufgabe eines Spielers.
-
-                Schachmatt: Ein Spieler setzt den gegnerischen König schachmatt, sodass dieser keinen legalen Zug mehr machen kann.
-                Remis: Es gibt verschiedene Möglichkeiten für ein Remis, z.B. durch dreifache Wiederholung der Zugfolge, unzureichendes Material oder durch Stellungswiederholung.
-
-                """;
-
-        JTextArea regelnTextArea = new JTextArea(regelnText);
-        regelnTextArea.setEditable(false);
-        regelnTextArea.setLineWrap(true);
-        regelnTextArea.setWrapStyleWord(true);
-        regelnTextArea.setBackground(new Color(255, 255, 0, 128)); // Hintergrundfarbe festlegen
-        JScrollPane regelnScrollPane = new JScrollPane(regelnTextArea);
-        regelnScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-        cardPanel.add(regelnScrollPane);
-
-        // Seite 3: Beschreibung der Figuren
+        // Seite 2: Beschreibung der Figuren
         String figurenText = """
                 Schachfiguren:
 
-                1. König: Der König ist die wichtigste Figur im Schach. Er kann ein Feld in jede Richtung ziehen.
+                1. König: 
+                   Der König ist die wichtigste Figur im Schach. Er kann ein Feld in jede Richtung ziehen.
                    Der König darf jedoch nicht auf ein Feld ziehen, das von einer gegnerischen Figur bedroht wird.
 
-                2. Dame: Die Dame ist die mächtigste Figur im Schach. Sie kann sich horizontal, vertikal und diagonal bewegen.
+                2. Dame: 
+                   Die Dame ist die mächtigste Figur im Schach. Sie kann sich horizontal, vertikal und diagonal bewegen.
                    Sie darf so viele Felder ziehen, wie sie möchte, solange keine Hindernisse im Weg sind.
 
-                3. Türme: Die Türme können sich horizontal und vertikal bewegen. Sie dürfen so viele Felder ziehen,
+                3. Türme: 
+                   Die Türme können sich horizontal und vertikal bewegen. Sie dürfen so viele Felder ziehen,
                    wie sie möchten, solange keine Hindernisse im Weg sind.
 
-                4. Läufer: Die Läufer bewegen sich diagonal. Sie dürfen so viele Felder ziehen, wie sie möchten,
+                4. Läufer: 
+                   Die Läufer bewegen sich diagonal. Sie dürfen so viele Felder ziehen, wie sie möchten,
                    solange keine Hindernisse im Weg sind.
 
-                5. Springer: Die Springer bewegen sich in einem L-förmigen Muster. Sie können zwei Felder geradeaus ziehen
+                5. Springer: 
+                   Die Springer bewegen sich in einem L-förmigen Muster. Sie können zwei Felder geradeaus ziehen
                    und dann ein Feld zur Seite oder ein Feld zur Seite und dann zwei Felder geradeaus.
 
-                6. Bauern: Die Bauern sind die schwächsten Figuren im Schach. Sie ziehen ein Feld nach vorne,
+                6. Bauern:  
+                   Die Bauern sind die schwächsten Figuren im Schach. Sie ziehen ein Feld nach vorne,
                    können aber beim ersten Zug auch zwei Felder vorrücken. Sie schlagen diagonal und können bei Erreichen
                    der gegnerischen Grundreihe in eine andere Figur umgewandelt werden.
 
@@ -226,12 +216,13 @@ public class MainFrame extends JFrame {
         figurenTextArea.setEditable(false);
         figurenTextArea.setLineWrap(true);
         figurenTextArea.setWrapStyleWord(true);
-        figurenTextArea.setBackground(new Color(255, 255, 0, 128)); // Hintergrundfarbe festlegen
+        figurenTextArea.setBackground(new Color(43, 43, 43)); // Hintergrundfarbe festlegen
+        figurenTextArea.setForeground(Color.white);
         JScrollPane figurenScrollPane = new JScrollPane(figurenTextArea);
         figurenScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         cardPanel.add(figurenScrollPane);
 
-        // Seite 4: Tipps und Tricks
+        // Seite 3: Tipps und Tricks
         String tippsText = """
                 Tipps und Tricks:
 
@@ -261,7 +252,8 @@ public class MainFrame extends JFrame {
         tippsTextArea.setEditable(false);
         tippsTextArea.setLineWrap(true);
         tippsTextArea.setWrapStyleWord(true);
-        tippsTextArea.setBackground(new Color(255, 255, 0, 128)); // Hintergrundfarbe festlegen
+        tippsTextArea.setBackground(new Color(43, 43, 43)); // Hintergrundfarbe festlegen
+        tippsTextArea.setForeground(Color.white);
         JScrollPane tippsScrollPane = new JScrollPane(tippsTextArea);
         tippsScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         cardPanel.add(tippsScrollPane);
